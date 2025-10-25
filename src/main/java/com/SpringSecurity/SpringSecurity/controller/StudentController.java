8c4f717f8cf892dbdb9ff106977cea8f84ca978e
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-10-25 10:11:14 +0530

    code

---
+++ src/main/java/com/SpringSecurity/SpringSecurity/controller/StudentController.java
@@ -0,0 +1,10 @@
+package com.SpringSecurity.SpringSecurity.controller;
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StudentController {
+
+
+}
