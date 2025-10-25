ec06ba04bad421a390fcd911a22eb04bc5488028
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-10-25 11:04:43 +0530

    code

--- src/main/java/com/SpringSecurity/SpringSecurity/config/SecurityConfig.java
+++ src/main/java/com/SpringSecurity/SpringSecurity/config/SecurityConfig.java
@@ -1,25 +1,68 @@
 package com.SpringSecurity.SpringSecurity.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    @Autowired
+    private UserDetailsService userDetailsSer;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
 
-        httpSecurity.csrf(customizer -> customizer.disable());
-        httpSecurity.authorizeHttpRequests(request -> request.anyRequest().authenticated());
+         return httpSecurity
+                .csrf(customizer -> customizer.disable())
+                .authorizeHttpRequests(request -> request.anyRequest().authenticated())
+//                .formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults())
+//                .logout(Customizer.withDefaults());
+                .sessionManagement(session->
+                session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
 
-        httpSecurity.formLogin(Customizer.withDefaults());
 
-        return httpSecurity.build();
+    }
+
+//    @Bean
+//    public UserDetailsService userDetailsService(){
+//
+//        UserDetails user1 = User.withDefaultPasswordEncoder()
+//                .username("kanahaiya")
+//                .password("k@123")
+//                .roles("USER")
+//                .build();
+//
+//        UserDetails user2 = User.withDefaultPasswordEncoder()
+//                .username("manoj")
+//                .password("m@123")
+//                .roles("ADMIN")
+//                .build();
+//
+//        return  new InMemoryUserDetailsManager(user1,user2);
+//    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(UserDetailsService userDetailsService){
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+        provider.setUserDetailsService(userDetailsSer);
+        return provider;
     }
 
 }
