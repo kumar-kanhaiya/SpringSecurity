26c1f8ce14b69a1590366667538916537119c06c
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-10-25 09:30:04 +0530

    Initial commit

---
+++ src/main/java/com/SpringSecurity/SpringSecurity/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.SpringSecurity.SpringSecurity.controller;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String greet(HttpServletRequest request){
+        return "Welcome to my website " + request.getSession().getId();
+    }
+}
